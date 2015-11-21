acd0bec6a966d801f60123b4c3d3d67724f53235
Celdir <michael.earl7769@gmail.com>
2015-11-21 16:57:57 -0600

    Added install.sh to initialize and start the server.

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt-get install git openjdk-7-jre-headless tar
+mkdir spigot
+cd spigot/
+curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+git config --global --unset core.autocrlf
+java -jar BuildTools.jar 
+echo "#/bin/sh" > start.sh
+sed -i '$ a\java -Xms512M -Xmx1024M -jar spigot-1.8.8.jar' start.sh
+chmod +x start.sh
+./start.sh 
+sed -i -- 's/false/true/g' eula.txt 
+./start.sh
