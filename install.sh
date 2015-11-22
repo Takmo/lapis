b0d5dc0335a7e045abfed3a91659ea791fac0c32
Celdir <michael.earl7769@gmail.com>
2015-11-21 20:51:26 -0600

    Updated install.sh to compile and install ShutdownManager plugin

--- install.sh
+++ install.sh
@@ -8,5 +8,12 @@ echo "#/bin/sh" > start.sh
 sed -i '$ a\java -Xms512M -Xmx1024M -jar spigot-1.8.8.jar' start.sh
 chmod +x start.sh
 ./start.sh 
-sed -i -- 's/false/true/g' eula.txt 
+sed -i -- 's/false/true/g' eula.txt
+cd ..
+git clone https://github.com/Takmo/lapis.git
+cd lapis/ShutdownManager
+cp ~/spigot/Spigot/Spigot-API/target/spigot-api-1.8.8-R0.1-SNAPSHOT.jar ~/lapis/ShutdownManager/spigot.jar
+ant jar
+cp server/plugins/ShutdownManager.jar ~/spigot/plugins/
+cd ~/spigot/
 ./start.sh
