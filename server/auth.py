03b3f5d8833130f9bd9db141b4697940ffec741b
Randall <takmo@bitwisehero.com>
2015-11-22 07:55:16 -0600

    Auth code should be finished (or at least almost so).

--- server/auth.py
+++ server/auth.py
@@ -9,19 +9,37 @@ def connect_sqlite():
 def auth_user(db, username, password):
     cursor = db.cursor()
     cursor.execute("SELECT * FROM users WHERE username=?", (username,))
+    if cursor.rowcount is 0:
+        print "User does not exist!"
+        return False
     row = cursor.fetchone()
+    if row is None:
+        print "User does not exist!"
+        return False
     if hash_password(password) == row[1]:
         return True
     return False
 
 def create_user(db, username, password):
     # Verify that user does not already exist.
-    for row in db.cursor().execute("SELECT * FROM users WHERE username=?", (username,)):
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users WHERE username=?", (username,))
+    if cursor.rowcount > 0:
         print "User already exists!"
         return False
     # Add them to database.
-    db.cursor().execute("INSERT INTO users (username, password, admin) " +
+    cursor.execute("INSERT INTO users (username, password, admin) " +
             "VALUES (?, ?, ?)", (username, hash_password(password), False))
+    db.commit()
+    return True
+
+def delete_user(db, username):
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users WHERE username=?", (username,))
+    if cursor.rowcount is 0:
+        print "User does not exist!"
+        return False
+    cursor.execute("DELETE FROM users WHERE username=?", (username,))
     db.commit()
     return True
 
@@ -36,4 +54,22 @@ def initialize_sqlite():
     c = connect_sqlite()
     c.cursor().execute("CREATE TABLE users (username VARCHAR(75), password VARCHAR(75), admin BOOLEAN)")
     c.close()
+
+def is_admin(db, username):
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users WHERE username=?", (username,))
+    if cursor.rowcount is 0:
+        print "User does not exist!"
+        return False
+    return (cursor.fetchone()[2] == 1)
+
+def set_admin(db, username, flag=1):
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users WHERE username=?", (username,))
+    if cursor.rowcount is 0:
+        print "User does not exist!"
+        return False
+    cursor.execute("UPDATE users SET admin=? WHERE username=?", (flag, username))
+    db.commit()
+    return True
 
