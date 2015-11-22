60ac6eaaf2d3b54d172e36d301b7a99052878792
Randall <takmo@bitwisehero.com>
2015-11-22 09:22:13 -0600

    Lots of changes to web service.

--- server/main.py
+++ server/main.py
@@ -1,42 +1,47 @@
 # See LICENSE file in top directory!
 
 import os
-import uuid
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+        render_template, flash, redirect
+from auth import auth_user, connect_sqlite
+from server import Server
 
+from uuid import uuid4
 
-# create our little application :)
+# The Flask app we'll be using.
 app = Flask(__name__)
+server = Server()
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'lapis.db'),
     DEBUG=False,
     SECRET_KEY=os.urandom(24),
 ))
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if not auth_user(username, password):
-            error = 'Invalid username or password'
+@app.route("/auth", methods=["GET", "POST"])
+def auth():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        con = connect_sqlite()
+        if not auth_user(con, username, password):
+            return redirect("/static/login.html?fail=1")
         else:
-            session['token'] = uuid4()
-            flash('You were logged in')
-            return redirect(url_for('main'))
-    return render_template('login.html', error=error)
+            token = uuid4()
+            session["token"] = token
+            server.insert_token(token, username)
+            return redirect("/")
+    return redirect("/static/login.html")
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    session.pop('token', None)
-    flash('You were logged out')
-    return redirect(url_for('login'))
+    session.pop("token", None)
+    return redirect("/static/login.html")
 
 @app.route('/')
 def main():
-    
+    return redirect("/static/login.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
