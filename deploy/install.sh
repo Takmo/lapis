6fbd07bcbd18a2b061fee36dbbbb727663e27687
BuildTools <unconfigured@null.spigotmc.org>
2015-11-22 03:13:28 +0000

    Updated install.sh to work based on new folder location assumptions

--- deploy/install.sh
+++ deploy/install.sh
@@ -11,10 +11,8 @@ sed -i '$ a\java -Xms512M -Xmx1024M -jar spigot-1.8.8.jar' start.sh
 chmod +x start.sh
 ./start.sh 
 sed -i -- 's/false/true/g' eula.txt
-cd ..
-git clone https://github.com/Takmo/lapis.git
-cd lapis/ShutdownManager
-cp ~/spigot/Spigot/Spigot-API/target/spigot-api-1.8.8-R0.1-SNAPSHOT.jar ~/lapis/ShutdownManager/spigot.jar
+cd ~/ShutdownManager
+cp ~/spigot/Spigot/Spigot-API/target/spigot-api-1.8.8-R0.1-SNAPSHOT.jar spigot.jar
 ant jar
 cp server/plugins/ShutdownManager.jar ~/spigot/plugins/
 cd ~/spigot/
